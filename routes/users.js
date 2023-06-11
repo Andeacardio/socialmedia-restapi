883eb51667217255f7fae1c8f458c5dcf9cbd0c8
Andeacardio <107910228+Andeacardio@users.noreply.github.com>
2023-06-11 21:09:25 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.put("/:id", async (req, res) => {
         $set: req.body,
       });
       res.status(200).json("Account has been updated");
-    } catch {
+    } catch (err){
       return res.status(500).json(err);
     }
   } else {
